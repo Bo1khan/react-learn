e897d8c9a9bb986de7b31645533053fc6b54a221
Boburkhon <Bo1khan@mail.ru>
2022-09-24 21:50:22 +0500

    006 lesson react

--- src/index.js
+++ src/index.js
@@ -2,20 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-// const elem = <h2>Hello World!</h2>;
-
-const text = 'Hello World!';
-
-const elem = (
-  <div>
-    <h2>Текст: {text}</h2>
-    <input type="text" />
-    <button>Click</button>
-  </div>
-);
+import {Header} from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App/>
 );
